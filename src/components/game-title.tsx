90c82150dd53ff768bd9dbffbb4b9c3415ea782d
LitovicMateo <litovicm@gmail.com>
2024-04-01 15:01:23 +0200

    path

--- src/components/game-title.tsx
+++ src/components/game-title.tsx
@@ -5,7 +5,7 @@ import TitleWord from "./title-word";
 const GameTitle = () => {
 	return (
 		<div className="flex gap-6 justify-center md:gap-8 md:justify-between w-[60%]">
-			<TitleWord image={"/TIC.png"} delay={2} />
+			<TitleWord image={"./TIC.png"} delay={2} />
 			<TitleWord image={Tac} delay={3} />
 			<TitleWord image={Toe} delay={4} />
 		</div>
